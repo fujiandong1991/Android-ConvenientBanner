aafb005172b90e2227ab9ff115c9b762c4302bf8
傅剑东 <fujiandong@thunder.com.cn>
2018-06-27 16:32:10 +0800

    PERF 优化代码格式

--- app/src/main/java/com/bigkoo/convenientbannerdemo/MainActivity.java
+++ app/src/main/java/com/bigkoo/convenientbannerdemo/MainActivity.java
@@ -1,18 +1,14 @@
 package com.bigkoo.convenientbannerdemo;
 
 import android.content.Intent;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
 import com.bigkoo.convenientbanner.ConvenientBanner;
+import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
 import com.bigkoo.convenientbanner.listener.OnItemClickListener;
 
 import java.lang.reflect.Field;
@@ -47,13 +43,13 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
     }
 
     private void initViews() {
-        convenientBanner = (ConvenientBanner) findViewById(R.id.convenientBanner);
+        convenientBanner = findViewById(R.id.convenientBanner);
     }
 
-    private void init(){
+    private void init() {
 //        initImageLoader();
         loadTestDatas();
-        //本地图片例子
+
         convenientBanner.setPages(
                 new CBViewHolderCreator() {
                     @Override
@@ -66,86 +62,20 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
                         return R.layout.item_localimage;
                     }
                 }, localImages)
-                //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
-//                .setPageIndicator(new int[]{R.drawable.ic_page_indicator, R.drawable.ic_page_indicator_focused})
                 .setOnItemClickListener(this);
-                //设置指示器的方向
-//                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT)
-//                .setOnPageChangeListener(this)//监听翻页事件
-                ;
-
-//        convenientBanner.setManualPageable(false);//设置不能手动影响
-
-        //网络加载例子
-//        networkImages=Arrays.asList(images);
-//        convenientBanner.setPages(new CBViewHolderCreator<NetworkImageHolderView>() {
-//            @Override
-//            public NetworkImageHolderView createHolder() {
-//                return new NetworkImageHolderView();
-//            }
-//        },networkImages);
 
 
-
-//手动New并且添加到ListView Header的例子
-//        ConvenientBanner mConvenientBanner = new ConvenientBanner(this,false);
-//        mConvenientBanner.setMinimumHeight(500);
-//        mConvenientBanner.setPages(
-//                new CBViewHolderCreator<LocalImageHolderView>() {
-//                    @Override
-//                    public LocalImageHolderView createHolder() {
-//                        return new LocalImageHolderView();
-//                    }
-//                }, localImages)
-//                //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
-//                .setPageIndicator(new int[]{R.drawable.ic_page_indicator, R.drawable.ic_page_indicator_focused})
-//                        //设置指示器的方向
-//                .setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT)
-//                .setOnItemClickListener(this);
-//        listView.addHeaderView(mConvenientBanner);
+        //设置两个点图片作为翻页指示器，不设置则没有指示器，可以根据自己需求自行配合自己的指示器,不需要圆点指示器可用不设
+        //.setPageIndicator(new int[]{R.drawable.ic_page_indicator, R.drawable.ic_page_indicator_focused})
+        //设置指示器的方向
+        //.setPageIndicatorAlign(ConvenientBanner.PageIndicatorAlign.ALIGN_PARENT_RIGHT)
+        //.setOnPageChangeListener(this)//监听翻页事件;
     }
 
-//    //初始化网络图片缓存库
-//    private void initImageLoader(){
-//        //网络图片例子,结合常用的图片缓存库UIL,你可以根据自己需求自己换其他网络图片库
-//        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().
-//                showImageForEmptyUri(R.drawable.ic_default_adimage)
-//                .cacheInMemory(true).cacheOnDisk(true).build();
-//
-//        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
-//                getApplicationContext()).defaultDisplayImageOptions(defaultOptions)
-//                .threadPriority(Thread.NORM_PRIORITY - 2)
-//                .denyCacheImageMultipleSizesInMemory()
-//                .diskCacheFileNameGenerator(new Md5FileNameGenerator())
-//                .tasksProcessingOrder(QueueProcessingType.LIFO).build();
-//        ImageLoader.getInstance().init(config);
-//    }
-    /*
-    加入测试Views
-    * */
     private void loadTestDatas() {
         //本地图片集合
         for (int position = 0; position < 7; position++)
             localImages.add(getResId("ic_test_" + position, R.drawable.class));
-
-
-////        //各种翻页效果
-//        transformerList.add(DefaultTransformer.class.getSimpleName());
-//        transformerList.add(AccordionTransformer.class.getSimpleName());
-//        transformerList.add(BackgroundToForegroundTransformer.class.getSimpleName());
-//        transformerList.add(CubeInTransformer.class.getSimpleName());
-//        transformerList.add(CubeOutTransformer.class.getSimpleName());
-//        transformerList.add(DepthPageTransformer.class.getSimpleName());
-//        transformerList.add(FlipHorizontalTransformer.class.getSimpleName());
-//        transformerList.add(FlipVerticalTransformer.class.getSimpleName());
-//        transformerList.add(ForegroundToBackgroundTransformer.class.getSimpleName());
-//        transformerList.add(RotateDownTransformer.class.getSimpleName());
-//        transformerList.add(RotateUpTransformer.class.getSimpleName());
-//        transformerList.add(StackTransformer.class.getSimpleName());
-//        transformerList.add(ZoomInTransformer.class.getSimpleName());
-//        transformerList.add(ZoomOutTranformer.class.getSimpleName());
-
-//        transformerArrayAdapter.notifyDataSetChanged();
     }
 
     /**
@@ -169,22 +99,22 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
     @Override
     protected void onResume() {
         super.onResume();
-//        //开始自动翻页
+        //开始自动翻页
         convenientBanner.startTurning();
     }
 
-     // 停止自动翻页
+    // 停止自动翻页
     @Override
     protected void onPause() {
         super.onPause();
-//        //停止翻页
+        // 停止翻页
         convenientBanner.stopTurning();
     }
 
 
     @Override
     public void onItemClick(int position) {
-        Toast.makeText(this,"点击了第"+position+"个",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "点击了第" + position + "个", Toast.LENGTH_SHORT).show();
         startActivity(new Intent(this, HeaderActivity.class));
 //        convenientBanner.setCanLoop(!convenientBanner.isCanLoop());
     }
